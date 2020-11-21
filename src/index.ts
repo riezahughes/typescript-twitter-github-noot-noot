4a3c0bb281ed6a7bfc2aa0f3b254a3a0f8cf0de9
DESKTOP-VN1U2BM\Dan <dan@ekwholesale.co.uk>
2020-11-21 20:10:34 +0000

    updates to search better

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Endpoints } from '@octokit/types';
 
 require('dotenv').config();
 
+const cron = require('node-cron');
+
 const { Octokit } = require('@octokit/rest');
 
 const { TwitterClient } = require('twitter-api-client');
@@ -13,28 +15,50 @@ const twitterClient = new TwitterClient({
   accessTokenSecret: process.env.ACCESSTOKENSECRET,
 });
 
-const test = async () => {
+const sendTweet = async () => {
   const octokit = new Octokit({
     userAgent: 'scotbot/rest.js v1.0.0',
   });
 
     type searchResponse = Endpoints['GET /search/issues']['response'];
 
+    const pageNumber: number = Math.floor(Math.random() * 5);
+
     await octokit.search.issuesAndPullRequests({
-      q: 'cunt',
-      s: 'created',
-      type: 'commits',
+      q: 'cunt+OR+wank+OR+bellend+OR+fanny+OR+bawbag+in:title',
+      sort: 'updated',
+      order: 'desc',
+      per_page: 100,
     }).then(async (result: searchResponse) => {
-      const chosenData = result.data.items[Math.floor(Math.random() * result.data.items.length)];
-      const { title, body, html_url } = chosenData;
+      //   console.log(result);
+      let found: boolean = false;
+      let title: String = '';
+      let html_url: String = '';
+      let body: String = '';
+
+      while (!found) {
+        const chosenData = result.data.items[Math.floor(Math.random() * result.data.items.length)];
+        console.log(chosenData);
+        if (chosenData.title.split(' ').length < 1) {
+          console.log('re-rollling...');
+        } else {
+          title = chosenData.title;
+          html_url = chosenData.html_url;
+          body = chosenData.body;
+          found = true;
+        }
+      }
+
       await twitterClient.tweets.statusesUpdate({
-        status: `title: ${title} - ${html_url} - ${body}`,
+        status: `${title} - ${html_url} - ${body.substring(0, 60)}...`,
       });
-      console.log(`title: ${title}`);
+      console.log(`${title}`);
       console.log(html_url);
       console.log(body);
     })
       .catch((err: any) => console.log(err));
 };
 
-test();
+cron.schedule('*/5 * * * *', () => {
+  sendTweet();
+});
